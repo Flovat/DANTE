1572e985cab78b0df26569625c7cabab2bea9279
Lorenzo Franceschi <lorenzofranceschi92@gmail.com>
2018-02-15 16:03:19 +0100

    Finished environment settings
    Added MySQL db, Apache server
    
    Added one new video and calulated arousal and valence with OpenFace

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $GLOBALS = array(
-    'db_host' => 'localhost',
+    'db_host' => 'localhost:3306',
     'db_name' => 'annotationdb',
-    'db_user' => '',
-    'db_pass' => '',
+    'db_user' => 'root',
+    'db_pass' => 'PippoeLollo#1',
 
     'anno_rate' => '25',    // get a value every 1/anno_rate seconds
-    'save_mode' => 'db'); // db or file
+    'save_mode' => 'file'); // db or file
 ?>
