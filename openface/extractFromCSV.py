a83b33b172d02436c5e84ca3dee52c864115a99e
Lorenzo Franceschi <lorenzofranceschi92@gmail.com>
2018-03-12 17:49:39 +0100

    Done the script to reduce all csv file

--- openface/extractFromCSV.py
+++ openface/extractFromCSV.py
@@ -1,15 +1,21 @@
 import csv
+import os
+import glob
 
-frame = list()
-timestamp = list()
-left_landmark = list()
-right_landmark = list()
-top_landmark = list()
-bottom_landmark = list()
-
-csv_list = list()
+path_store = 'D:/Projects/DANTE/openface/*.csv'
+path_save = 'D:/Projects/DANTE/openface/csv/'
 
 def readFromCSV(spamReader):
+
+    frame = list()
+    timestamp = list()
+    left_landmark = list()
+    right_landmark = list()
+    top_landmark = list()
+    bottom_landmark = list()
+
+    csv_list = list()
+
     #leggi csv e crea struttura dati per salvare i valori
     for row in spamReader:
         #num of frame
@@ -33,10 +39,12 @@ def readFromCSV(spamReader):
     csv_list.append(top_landmark)
     csv_list.append(bottom_landmark)
 
-def writeIntoCSV(file_name):
+    return len(frame), csv_list
+
+def writeIntoCSV(file_name, dim, csv_list):
     #scrivi nuovo csv
-    with open(file_name + '_small.csv', 'w') as outfile:
-        dim = len(frame)
+    with open(path_save + 'reduced_' + file_name, 'w') as outfile:
+
         #ciclo su tutti i frame
         for i in range(0, dim):
             #controllo sul primo valore pre non mettere il carattere ','
@@ -44,24 +52,48 @@ def writeIntoCSV(file_name):
             #ciclo sugli array all'interno di csv_list (ovvero tutti i valori che mi servono)
             for entries in csv_list:
                 if k!=0:
-                    outfile.write(',')
+                    outfile.write(',')  
                 outfile.write(entries[i])
                 k+=1
             outfile.write('\n')
 
+def alreadyExists(file_name):
+    for xpath in glob.glob(path_save + 'reduced_' + file_name): 
+        tmp = 'reduced_'+ file_name
+        if tmp == os.path.basename(xpath):
+            return False
+    return True
+           
+                
+            
 def main():
-    #specifica il filename del tuo csv che vuoi rimpiccolire
-    file_name = input('Inserisci il nome del file: ')
 
-    #creo l' oggetto reader per lettura da file .csv
-    spamReader = csv.reader(open(file_name + '.csv', newline=''))
+    #glob.glob(path_store) è un metodo di blob che rutorna tutti i path dei file in path_store
+    for path_name in glob.glob(path_store):
 
-    #leggo
-    readFromCSV(spamReader)
+        #tiro fuori il nome del file senza il path
+        file_name = os.path.basename(path_name)
 
-    #scrivo
-    writeIntoCSV(file_name)
+        if alreadyExists(file_name): 
+        
+            print(os.path.basename(file_name) + ' opened')
+            #creo l' oggetto reader per lettura da file .csv
+            spamReader = csv.reader(open(path_name, newline=''))
 
+            #leggo
+            dim, csv_list = readFromCSV(spamReader)
+
+            #scrivo
+            writeIntoCSV(file_name, dim, csv_list)
+
+            print(os.path.basename('reduce file completed, total frame: ' + str(dim) + '\n'))
+        
+        else:
+            print('\n' + os.path.basename(file_name) + ' already restricted, delete the reduced file to recalculate it')
+                
+            
+                
+            
 main()
 
 
